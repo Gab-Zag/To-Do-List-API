80826d13889caa4c4a8d8fb4c07b94849f993e45
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2026-02-16 18:14:47 -0300

    feat:Create base project

---
+++ src/test/java/com/todolist/tdl/TdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.tdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
